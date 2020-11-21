f00db95fb8240d5c936e332d06ef8ab28a730c90
Muhsin Wahi-Anwar <mkw5629@psu.edu>
2020-11-21 17:55:51 -0500

    Implemented basic functionality for the viewcontroller and storyboard.

--- tipcalculatorapp/ViewController.swift
+++ tipcalculatorapp/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // More setup
+        
     }
-
-
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func tipChangeAction(_ sender: Any) {
+        // Here we will calculate the tip and total bill
+        // And display it
+        let bill = Double(textField.text!) ?? 0
+        let tipPercentages = [0.15, 0.20, 0.25]
+        
+        // calulating tips
+        let tip = bill * tipPercentages[tipSelector.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Changing labels to reflect values
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalAmountLabel.text = String(format: "$%.2f", total)
+    }
 }
-
